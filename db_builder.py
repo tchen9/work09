f764eb05b4c4f45ac8df3f49cc6d0f7b7f501d92
Tina <tchen9@stuy.edu>
2017-10-14 23:46:32 -0400

    udpated code

--- db_builder.py
+++ db_builder.py
@@ -10,6 +10,22 @@ c = db.cursor()    #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
+#adds values from courses
+
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);") #course table
+with open('courses.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute('INSERT INTO courses VALUES("' + "row['code']" + '", ' + "row['mark']" + ", " + "row['id']" + "); ")
+
+#added values from peeps
+c.execute("CREATE TABLE peeps (name TEXT, age INTEGER, id INTEGER)")
+with open('peeps.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute('INSERT INTO peeps VALUES("' + "row['name']" + '", ' + "row['age']" + ", " + "row['id']" + ');')
+
+                  
 
 command = ""          #put SQL statement in this string
 c.execute(command)    #run SQL statement
